4775478ef8d27957037230f63f6f7b902703b651
Volkov Alexander <volkov.525@gmail.com>
2020-10-01 15:54:53 +0300

    made checkbox indeterminate work

--- src/components/listTable.vue
+++ src/components/listTable.vue
@@ -4,12 +4,12 @@ v-simple-table(fixed-header height="400").mytab
 		thead
 			tr.head
 				th.sm
-					v-simple-checkbox.check
+					v-simple-checkbox(v-model="allSelected" color="primary" :indeterminate="indeterminate" v-ripple).check
 				th(v-for="header in headers") {{ header.text }}
 		tbody
 			tr(v-for="(item, index) in items" :key="index").slide
 				td.sm
-					v-simple-checkbox(v-model="selection[index]" color="primary").check
+					v-simple-checkbox(v-model="selection[index]" color="primary" v-ripple).check
 				td fuck
 				td fuck
 				td fuck
@@ -25,7 +25,20 @@ export default {
 		headers,
 		items,
 		selection: [],
+		allSelected: false,
 	}),
+	computed: {
+		indeterminate () {
+			let sel = this.selection.reduce((total, item) => {
+				if (item === true) {
+					return total += 1
+				} else return total
+			}, 0)
+			if (sel > 0) {
+				return true
+			} else return false
+		}
+	}
 }
 
 </script>
